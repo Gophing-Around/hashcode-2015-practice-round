9af441f1d5354394bafaa475cf999c78599d12f4
roberto.stagi <roberto.stagi@mia-platform.eu>
2022-02-04 15:12:01 +0100

    add rows

--- algorithm.go
+++ algorithm.go
@@ -4,17 +4,24 @@ import "fmt"
 
 func algorithm(config Config, unMap unavailablesMap, servers []*Server) int {
 	// occupiedMap := make(unavailablesMap, 0)
+	rows := make([]*Row, config.rows)
 	for sPos := 0; sPos < len(servers); sPos++ {
 		server := servers[sPos]
 		fmt.Printf("Working for server %d - %+v\n", sPos, server)
 		fmt.Printf("UN MAP %+v\n", unMap)
-		placeServer(config, unMap, server, sPos)
+		placeServer(config, unMap, server, sPos, rows)
 	}
+
+	fmt.Printf("%+v\n", rows)
 
 	return 42
 }
 
-func placeServer(config Config, unMap unavailablesMap, server *Server, sPos int) {
+type Row struct {
+	servers []*Server
+}
+
+func placeServer(config Config, unMap unavailablesMap, server *Server, sPos int, rows []*Row) {
 	for i := 0; i < config.rows; i++ {
 		for j := 0; j < config.slots; j++ {
 			if j+server.size > config.slots {
@@ -37,8 +44,8 @@ func placeServer(config Config, unMap unavailablesMap, server *Server, sPos int)
 
 			// Slots unavailable
 			fmt.Printf("BEFORE ITERATING FOR SERVER %d - %+v\n", sPos, unMap)
+			rows[i].servers = append(rows[i].servers, server)
 			for k := j; k < j+server.size && k < config.slots; k++ {
-
 				fmt.Printf("SETTING TRUE %d %d\n", i, k)
 				unMap[fmt.Sprintf("%d %d", i, k)] = true
 			}
